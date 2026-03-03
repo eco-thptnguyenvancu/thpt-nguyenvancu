d100380e614701863006cca73152b0555bedeffc
eco-thptnguyenvancu <anyhume0414@gmail.com>
2026-03-03 10:58:09 +0700

    cập nhật nội dung mới

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
@@ -5,6 +6,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base:'/thpt-nguyenvancu',
       server: {
         port: 3000,
         host: '0.0.0.0',
@@ -18,6 +20,7 @@ export default defineConfig(({ mode }) => {
         alias: {
           '@': path.resolve(__dirname, '.'),
         }
+        
       }
     };
 });
